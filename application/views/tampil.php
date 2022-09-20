632cce85d939059dffc2d80bae8de386191787fd
widaaryanti <widaaryanti13@gmail.com>
2022-09-20 16:47:17 +0700

    first commit

---
+++ application/views/tampil.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Latihan Pertama</title>
+</head>
+<body>
+    <p>Latihan pertama saya belajar framework CodeIgniter</p>
+    <p>Framework sangatt mudah dipelajari</p>
+
+</body>
+</hmtl>
